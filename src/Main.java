c68000cabd6ec17b9358b39a5184d855b5af0852
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-01 17:02:05 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 565;
+        byte b = -121;
+        short c = 13;
+        long d = 11222333L;
+        float f = 25.16f;
+        double g = 45.3222;
+        System.out.println("Значение переменной a с типом int равно " + a );
+        System.out.println("Значение переменной b с типом byte равно " + b );
+        System.out.println("Значение переменной c с типом short равно " + c );
+        System.out.println("Значение переменной d с типом long равно " + d );
+        System.out.println("Значение переменной f с типом float равно " + f );
+        System.out.println("Значение переменной g с типом double равно " + g );
+
+
+
     }
 }
